00b3af309686c9a0dc7b2b66c894a388647320d4
AR <ar.anthonyrobin@gmail.com>
2020-02-10 14:58:14 +0100

    MAJ sql

--- ROBIN_Anthony.sql
+++ ROBIN_Anthony.sql
@@ -1,2 +1,53 @@
-select count(*)
-from R;
+drop table if exists TWITTER;
+
+create table if not exists TWITTER (
+  user int,
+  follower int
+);
+
+.separator ' '
+
+.import 'twitter.edgelist' TWITTER
+
+select "nb total de relations friend/follower : ", count(*)
+from TWITTER;
+
+select "nb utilisateurs qui ont au moins un follower : ", count(distinct(user))
+from TWITTER;
+
+select "nb utilisateurs qui suivent au moins qqn : ", count(distinct(follower))
+from TWITTER;
+
+select "nb max de followers par utilisateur : ", max(foo.cpt)
+from (
+  select count(*) as cpt
+  from TWITTER
+  group by user
+) as foo;
+
+select "-- exemple utilisateur avec nb max de followers : ", foo.user
+from (
+  select max(foo.cpt), foo.user
+  from (
+    select count(*) as cpt, user
+    from TWITTER
+    group by user
+  ) as foo
+) as foo;
+
+select "nb min de followers par utilisateur : ", min(foo.cpt)
+from (
+  select count(*) as cpt
+  from TWITTER
+  group by user
+) as foo;
+
+select "-- exemple utilisateur avec nb min de followers : ", foo.user
+from (
+  select min(foo.cpt), foo.user
+  from (
+    select count(*) as cpt, user
+    from TWITTER
+    group by user
+  ) as foo
+) as foo;
